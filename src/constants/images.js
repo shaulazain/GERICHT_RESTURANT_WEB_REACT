370a8ca4ae7700e67f9bb33ae789a6e58c4455ba
Shahzaib <shaulazain@gmail.com>
2024-03-28 19:21:23 +0500

    first commit

---
+++ src/constants/images.js
@@ -0,0 +1,47 @@
+import bg from '../assests/bg.png';
+import chef from '../assests/chef.png';
+import G from '../assests/G.png';
+import gallery01 from '../assests/gallery01.png';
+import gallery02 from '../assests/gallery02.png';
+import gallery03 from '../assests/gallery03.png';
+import gallery04 from '../assests/gallery04.png';
+import knife from '../assests/knife.png';
+import logo from '../assests/logo.png';
+import menu from '../assests/menu.png';
+import overlaybg from '../assests/overlaybg.png';
+import spoon from '../assests/spoon.svg';
+import welcome from '../assests/welcome.png';
+import findus from '../assests/findus.png';
+import laurels from '../assests/laurels.png';
+import award01 from '../assests/award01.png';
+import award02 from '../assests/award02.png';
+import award03 from '../assests/award03.png';
+import award05 from '../assests/award05.png';
+import sign from '../assests/sign.png';
+import quote from '../assests/quote.png';
+import gericht from '../assests/gericht.png';
+
+export default {
+  bg,
+  chef,
+  G,
+  gallery01,
+  gallery02,
+  gallery03,
+  gallery04,
+  knife,
+  logo,
+  menu,
+  overlaybg,
+  spoon,
+  welcome,
+  findus,
+  laurels,
+  award01,
+  award02,
+  award03,
+  award05,
+  sign,
+  quote,
+  gericht,
+};
